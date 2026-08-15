f34a94efabf97eb0b308b414720cc4304ee3e3bf
Charlotte Moche <charlottebmoche@gmail.com>
2026-08-15 08:35:00 -0400

    feat: add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <></>
+export default function App() {
+  return (
+    <>
+      <nav className="flex items-center justify-between p-4">
+        <ul className="m-0 list-none p-0">
+          <li>
+            <a href="/">
+              <img
+                src="/charlotte-truck.png"
+                alt="Logo"
+                className="h-10 w-10 lg:h-12 lg:w-12"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
-
-export default App
